dd2666cbdb84a15fb987c6fe0a8c00977a5cefa2
Ajmal Muhammed <muhammedajmal54@gmail.com>
2024-06-05 11:04:16 +0530

    authenticating the user existence in logged in middleware

--- src/middlewares/is-logged-in.ts
+++ src/middlewares/is-logged-in.ts
@@ -2,8 +2,14 @@ import jwt from 'jsonwebtoken'
 import { envVariables } from '../config/initilize-env-variables-config'
 import { NextFunction, Request, Response } from 'express'
 import CustomError from './error'
+import { connectDB } from '../config/db-config'
+import { User } from '../entities/user-entity'
 
-export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
+export const isLoggedIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (!req.cookies.token) {
     return next(new CustomError('AuthenticationError'))
   }
@@ -11,11 +17,14 @@ export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
     const user: any = jwt.verify(req.cookies.token, envVariables.JWT_SECRET)
 
     const { emailId, userId } = user
-    if (emailId !== req.body.emailId || userId !== req.body.userId) {
+    const authenticatedUser = await connectDB.getRepository(User).findOne({
+      select: ['id', 'emailId'],
+      where: { id: userId, emailId: emailId },
+    })
+    if (!authenticatedUser) {
       return next(new CustomError('AuthenticationError', 401))
     }
-    req.body.id = userId
-    req.body.emailId = emailId
+    req.body.authenticatedUser = authenticatedUser
   } catch (err) {
     return next(err)
   }
