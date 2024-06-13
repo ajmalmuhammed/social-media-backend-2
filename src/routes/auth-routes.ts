b6f2e492625d2a024a81f8aafdddcf0150b42ee2
Ajmal Muhammed <muhammedajmal54@gmail.com>
2024-06-13 20:03:39 +0530

    added logout route and postman collection for testing

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -4,7 +4,7 @@ import { verifyOTP } from '../controllers/otp-controller'
 
 const router = express.Router()
 router.post('/login', login)
-// router.get('/logout',logout);
+router.get('/logout', logout)
 router.post('/verify', verifyOTP)
 router.post('/register', register)
 export default router
